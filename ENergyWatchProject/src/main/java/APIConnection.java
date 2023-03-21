5c4b015719de3af257cf3a0fdc6c5313b8137e13
Nikolaj <nthom21@student.sdu.dk>
2023-03-21 11:19:58 +0100

    Working APIConnection. Printer JSON ud, men output skal nok ændres

---
+++ ENergyWatchProject/src/main/java/APIConnection.java
@@ -0,0 +1,38 @@
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class APIConnection {
+    public static void main(String[] args) {
+        try {
+            // set the API URL. Data fra starten af året til nu. Priserne time for time.
+            URL url = new URL("https://api.energidataservice.dk/dataset/Elspotprices?start=now-P1Y&end=now&filter={\"PriceArea\":[\"DK1\",\"NO\"]}&limit=400");
+
+            // create a connection to the API endpoint
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+
+            // setup connection parameters
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Content-Type", "application/json"); //response content type
+            conn.setConnectTimeout(5000);
+            conn.setReadTimeout(5000);
+
+
+            // read the API response
+            BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+
+            // print the API response
+            System.out.println(response.toString());
+        } catch (Exception e) {
+            System.out.println("Exception: " + e.getMessage());
+        }
+    }
+}
